262a2e087bb2ced12d9e51060178b0711279b300
Daniel Bariche <dabar@kth.se>
2025-03-12 21:59:22 +0100

    Require robot to be placed before move or rotation

--- robot.py
+++ robot.py
@@ -17,14 +17,18 @@ class Robot:
         self.y = None
         self.direction = None
         self.table = table
+        self.is_placed = False
 
     def place(self, x, y, direction):
         if self.table.is_valid_placement(x, y, direction):
             self.x = x
             self.y = y
             self.direction = direction
+            self.is_placed = True
 
     def move(self):
+        if not self.is_placed:
+            return
         step_x, step_y = MOVEMENTS[self.direction]
         tmp_x = self.x + step_x
         tmp_y = self.y + step_y
@@ -33,19 +37,26 @@ class Robot:
             self.y = tmp_y
 
     def right(self):
+        if not self.is_placed:
+            return
         self.direction = RIGHT_TURN[self.direction]
 
     def left(self):
+        if not self.is_placed:
+            return
         self.direction = LEFT_TURN[self.direction]
 
     def report(self):
+        if not self.is_placed:
+            return
         return f"{self.x},{self.y},{self.direction}"
 
 
 if __name__ == "__main__":
     t = Table()
     r = Robot(t)
-    r.place(10, 20, "SOUTH")
-    print(r.report())
-    r.place(1, 2, "SOUTH")
+    r.x = 1
+    r.y = 2
+    r.direction = "NORTH"
+    r.move()
     print(r.report())
